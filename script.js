6a9efc38fe3e9ec61bbd602fe075d4adcc631c53
Aryan Jha <115208575+Aryanjha433@users.noreply.github.com>
2023-08-10 13:44:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("active");
+}
+document.querySelector("#root > div.App > div > header > div.App-header.small > div > div.App-header-container-right > div > div.App-header-network > div")
